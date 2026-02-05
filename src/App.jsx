b6589450805e463043ebed78745d1344d9c1018f
Telax4As <128410986+Telax4As@users.noreply.github.com>
2026-02-05 20:59:15 +0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App() {
   const toggleAll = () => {
     setTodos(
       todos.map(todo => 
-        ({...todo, completed: !todo.completed})
+        ({...todo, completed: true})
       )
     )
   }
